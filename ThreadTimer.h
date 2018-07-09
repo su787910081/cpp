2b6de2e0380d83c5bb36243a289c5e1a52a7e7d2
su787910081 <su_787910081@sina.com>
2018-07-09 17:27:44 +0800

    Update ThreadTimer.h
    中国时区

--- ThreadTimer.h
+++ ThreadTimer.h
@@ -13,28 +13,33 @@ class OnceTimer;
 
 class ThreadTimer
 {
-    public: 
-        void Start();
-        void Stop();
-        
-        void AsyncTimerKeepLine();
-        /// 添加一个从当前时间延时指定时间操作，单位：毫秒。
-        /// 返回定时器ID
-        uint64_t AddExpiresFromNow(std::size_t ms, std::function<void(unit64_t)> cb);
-        /// 从一个指定时刻延时指定时间的操作，单位：毫秒
-        /// 返回定时器ID
-        uint64_t AddExpiresFromAt(const std::string &fmtDateTime, 
-            std::size_t ms, std::function<void(uint64_t)>cb);
-        /// 添加一个指定时间处理的操作，只精确到秒
-        /// fmtDateTime(YYYY-MM-DD HH:MM:SS)、(YYYYMMDDTHHMMSS)
-        /// 返回定时器ID
-        uint64_t AddExpiresAt(const std::string &fmtDateTime, 
-            std::function<void(uint64_t)> cb);
-        uint64_t AddExpiresAt(time_t atTime, std::function<void(uint64_t)> cb);
-        uint64_t AddExpiresAt(const boost::posix_time::ptime &tm, 
-            std::function<void(uint64_t)> cb);
-        std::size_t DynamicTimerSize();
-        
+public:
+    // 中国时区 - 东八区
+    using CH_ZONE = boost::date_time::local_adjustor<
+            boost::posix_time::ptime, +8, boost::posix_time::no_dst>;
+    
+public: 
+    void Start();
+    void Stop();
+    
+    void AsyncTimerKeepLine();
+    /// 添加一个从当前时间延时指定时间操作，单位：毫秒。
+    /// 返回定时器ID
+    uint64_t AddExpiresFromNow(std::size_t ms, std::function<void(unit64_t)> cb);
+    /// 从一个指定时刻延时指定时间的操作，单位：毫秒
+    /// 返回定时器ID
+    uint64_t AddExpiresFromAt(const std::string &fmtDateTime, 
+        std::size_t ms, std::function<void(uint64_t)>cb);
+    /// 添加一个指定时间处理的操作，只精确到秒
+    /// fmtDateTime(YYYY-MM-DD HH:MM:SS)、(YYYYMMDDTHHMMSS)
+    /// 返回定时器ID
+    uint64_t AddExpiresAt(const std::string &fmtDateTime, 
+        std::function<void(uint64_t)> cb);
+    uint64_t AddExpiresAt(time_t atTime, std::function<void(uint64_t)> cb);
+    uint64_t AddExpiresAt(const boost::posix_time::ptime &tm, 
+        std::function<void(uint64_t)> cb);
+    std::size_t DynamicTimerSize();
+    
 private:
     void DoTimerKeepLive();
     
