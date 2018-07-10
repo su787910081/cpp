e25405754f1b901ff89196186cbb844c4ddf1e90
su787910081 <su_787910081@sina.com>
2018-07-10 16:29:49 +0800

    Create TimeStdCpp.cpp
    标准C++ 库中的时间函数 调用示例

---
+++ TimeStdCpp.cpp
@@ -0,0 +1,69 @@
+
+void current_time_01()
+{
+    // 当前时间点
+    std::chrono::system_clock::time_point t_now =
+        std::chrono::high_resolution_clock::now();
+    // 转换精度到微秒级别
+    std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds> t_n = 
+        std::chrono::time_point_cast<std::chrono::microseconds>(t_now);
+
+    time_t nt = std::chrono::system_clock::to_time_t(t_now);
+    struct tm btm;
+#ifdef _WIN32
+    localtime_s(&btm, &nt);
+#else
+    localtime_r(&nt, &btm);
+#endif
+
+    char mbstr[256] = { 0 };
+    strftime(mbstr, sizeof(mbstr), "%Y-%m-%d %H:%M:%S", &btm);
+    printf("%s.%06" PRIu64 "\n", mbstr, t_n.time_since_epoch() % 1000000);
+}
+
+#include <sys/timeb.h>
+
+char* cur_time_02(char strDateTime[32])
+{
+    struct timeb tp_cur;
+    
+    ftime(&tp_cur);
+    
+    struct tm btm;
+    
+#ifdef _WIN32
+    localtime_s(&btm, &tp_cur.time);
+#else
+    localtime_r(&tp_cur.time, &btm);
+#endif
+    
+    sprintf(strDateTime, "%04d-%02d-%02d %02d:%02d:%02d.%03d",
+            btm.tm_year + 1900, btm.tm_mon + 1, btm.tm_mday,
+            btm.tm_hour, btm.tm_min, btm.tm_sec, 
+            tp_cur.millitm);
+    
+    return strDateTime;
+}
+
+extern char* cur_time_03(char strDateTime[32])
+{
+    static const std::chrono::hours EIGHT_HOURS(8);
+    
+    // +8 小时转换为北京时间
+    auto nowLocalTimeCount = std::chrono::system_clock::now().time_since_epock() + EIGHT_HOURS;
+    
+    std::chrono::hours now_h = std::chrono::duration_cast<
+        std::chrono::hours>(nowLocalTimeCount);
+    std::chrono::minutes now_m = std::chrono::duration_cast<
+        std::chrono::minutes>(nowLocalTimeCount);
+    std::chrono::seconds now_s = std::chrono::duration_cast<
+        std::chrono::seconds>(nowLocalTimeCount);
+    std::chrono::milliseconds now_ms = std::chrono::duration_cast<
+        std::chrono::milliseconds>(nowLocalTimeCount);
+    
+    sprintf(strDateTime, "%02d:%02d:%02d.%03d",
+            now_h % 24, now_m % 60, now_s % 60, now_ms % 1000);
+    
+    return strDatetime;
+}
+
