791fd45f7271ba8459b18da4ba17f0c9a6dd8e80
su787910081 <su_787910081@sina.com>
2018-07-10 16:53:05 +0800

    Create BoostString.cpp

---
+++ SmallExample/BoostExample/BoostString.cpp
@@ -0,0 +1,12 @@
+#include <boost/algorithm/string.hpp>
+
+void boostSplit()
+{
+    std::string strSrc = "1,3,5,  66cdd,2,3,4 5 ,";
+    
+    // boost::token_compress_on: 将连续多个分隔符当成一个分隔符处理。
+    // 即：连续多个分隔符之间不会生成一个"" 字符 串。如",,, " 只会有一个""
+    boost::split(vecResult, strSrc, boost::is_any_of(", "),
+        boost::token_compress_on);
+    
+}
